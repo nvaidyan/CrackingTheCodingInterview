8af89859475822e970171753a554cbf33aaaddb4
Nick Vaidyanathan <visionary.software.solutions@gmail.com>
2014-09-13 15:18:24 -0700

    added tests for 1.3

--- tst/info/nicholasvaidyanathan/crackingthecodinginterview/ch1/StringDeduperTest.java
+++ tst/info/nicholasvaidyanathan/crackingthecodinginterview/ch1/StringDeduperTest.java
@@ -18,4 +18,16 @@ public class StringDeduperTest {
         toTest = new StringDecorator("the quick brown fox jumped over the lazy dog");
         assertEquals("the quickbrownfxjmpdvlazyg", toTest.removeDuplicateCharacters());
     }
+
+    @Test
+    public void handlesContiguousChars() {
+        toTest = new StringDecorator("aaaaaabbbbbb");
+        assertEquals("ab", toTest.removeDuplicateCharacters());
+    }
+
+    @Test
+    public void handlesNonContiguousChars() {
+        toTest = new StringDecorator("abababababababa");
+        assertEquals("ab", toTest.removeDuplicateCharacters());
+    }
 }
